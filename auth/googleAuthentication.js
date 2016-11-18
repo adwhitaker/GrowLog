08e2a0676c3c30e3c94501e64d67da3e04771a5e
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-18 15:59:49 -0600

    google authentication routes

---
+++ auth/googleAuthentication.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+router.get('/',
+  passport.authenticate('google', {
+    scope: ['profile'],
+    prompt: 'select_account',
+    accessType: 'offline'
+  }
+));
+
+router.get('/callback',
+  passport.authenticate('google', {
+    successRedirect: '/home',
+    failureRedirect: '/auth/google/failure'
+  }
+));
+
+module.exports = router;
