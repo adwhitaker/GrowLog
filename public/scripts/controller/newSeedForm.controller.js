3000fdbb3160bec9872fe0a85754ff7b9ac302b2
elisa lee <elisa.my.lee@gmail.com>
2016-11-19 13:12:22 -0600

    add NewSeedForm controller

---
+++ public/scripts/controller/newSeedForm.controller.js
@@ -0,0 +1,7 @@
+angular.module('growLogApp')
+       .controller('NewSeedFormController', NewSeedFormController);
+
+function NewSeedFormController() {
+  var newSeed = this;
+  console.log('NewSeedFormController loaded');
+}
