8f521e69de6b98c8f6d7550929fc92d2f33f6db1
elisa lee <elisa.my.lee@gmail.com>
2016-11-19 12:07:56 -0600

    fix routeProvider

--- public/scripts/config/main.config.js
+++ public/scripts/config/main.config.js
@@ -1,33 +1,41 @@
 angular.module('growLogApp')
        .config(configVeggies);
 
-function configVeggies($routerProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-
-  $routerProvider.when('/home', {
+function configVeggies($routeProvider, $locationProvider) {
+  $routeProvider.when('/home', {
     templateUrl: 'views/home.html',
     controller: 'HomeController as home',
   }).when('/seeds', {
     templateUrl: 'views/seeds.html',
-    controller: 'SeedsController as seeds'
+    controller: 'SeedsController as seeds',
   }).when('/activity', {
     templateUrl: 'views/activity.html',
+    controller: 'ActivityController as activity',
   }).when('/plant', {
     templateUrl: 'views/plant.html',
-  }).when('/inputs', {
-    templateUrl: 'views/inputs.html',
+    controller: 'PlantController as plant',
+  }).when('/water', {
+    templateUrl: 'views/water.html',
+    controller: 'WaterController as water',
   }).when('/weed', {
     templateUrl: 'views/weed.html',
+    controller: 'WeedController as weed',
   }).when('/harvest', {
     templateUrl: 'views/harvest.html',
+    controller: 'HarvestController as harvest',
   }).when('issues', {
     templateUrl: 'views/issues.html',
+    controller: 'IssuesController as issues',
   }).when('/other', {
     templateUrl: 'views/other.html',
+    controller: 'OtherController as other',
   }).when('/', {
     templateUrl: 'views/login.html',
-    controller: 'LoginController as login'
+    controller: 'LoginController as login',
   }).when('/generatereport', {
     templateUrl: 'views/generatereport.html',
-  }).otherwise();
-};
+    controller: 'ReportController as report',
+  });
+
+  $locationProvider.html5Mode(true);
+}
