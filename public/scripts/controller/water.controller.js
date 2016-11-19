b8d446ea0320065cd1e4b592d66e13fac28528cc
elisa lee <elisa.my.lee@gmail.com>
2016-11-19 12:14:25 -0600

    add water controller

---
+++ public/scripts/controller/water.controller.js
@@ -0,0 +1,7 @@
+angular.module('growLogApp')
+       .controller('WaterController', WaterController);
+
+function WaterController() {
+  var water = this;
+  console.log('WaterController loaded');
+}
