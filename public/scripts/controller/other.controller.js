554b959705e8652ff44d2de4e623380795170a86
elisa lee <elisa.my.lee@gmail.com>
2016-11-19 12:13:20 -0600

    add other controller

---
+++ public/scripts/controller/other.controller.js
@@ -0,0 +1,7 @@
+angular.module('growLogApp')
+       .controller('OtherController', OtherController);
+
+function OtherController() {
+  var issues = this;
+  console.log('OtherController loaded');
+}
