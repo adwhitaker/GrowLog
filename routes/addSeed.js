60183a79f148be4add381d1da96ee95f188cd32e
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-19 11:52:16 -0600

    get seed from DB

---
+++ routes/addSeed.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const config = require('../db/connections');
+const knex = require('knex')(config.development);
+
+router.route('/')
+      .get(getSeeds)
+      .post(addSeeds);
+
+router.route('/:id')
+      .put(updateSeeds)
+      .delete(deleteSeed);
+
+function getSeeds(req, res) {
+
+  knex.select()
+      .from('seeds')
+      .then(function (response) {
+        console.log('response', response);
+        res.send(response);
+      }).catch(function (err) {
+        console.log('Error Querying the DB', err);
+        res.sendStatus(500);
+      });
+};
+
+function addSeeds(req, res) {
+
+};
+
+function updateSeeds(req, res) {
+
+}
+
+function deleteSeed(req, res) {
+
+};
+
+
+module.exports = router;
