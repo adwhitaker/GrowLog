57abf479b0365305eea4ded89fbd5bb4fc2edced
elisa lee <elisa.my.lee@gmail.com>
2016-11-19 10:54:08 -0600

    add a config folder and move file

---
+++ public/scripts/config/main.config.js
@@ -0,0 +1,33 @@
+angular.module('growLogApp')
+       .config(configVeggies);
+
+function configVeggies($routerProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
+  $routerProvider.when('/home', {
+    templateUrl: 'views/home.html',
+    controller: 'HomeController as home',
+  }).when('/seeds', {
+    templateUrl: 'views/seeds.html',
+    controller: 'SeedsController as seeds'
+  }).when('/activity', {
+    templateUrl: 'views/activity.html',
+  }).when('/plant', {
+    templateUrl: 'views/plant.html',
+  }).when('/inputs', {
+    templateUrl: 'views/inputs.html',
+  }).when('/weed', {
+    templateUrl: 'views/weeds.html',
+  }).when('/harvest', {
+    templateUrl: 'views/harvest.html',
+  }).when('issues', {
+    templateUrl: 'views/issues.html',
+  }).when('/other', {
+    templateUrl: 'views/other.html',
+  }).when('/', {
+    templateUrl: 'views/login.html',
+    controller: 'LoginController as login'
+  }).when('/generatereport', {
+    templateUrl: 'views/generatereport.html',
+  }).otherwise();
+};
