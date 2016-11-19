91de3b796e8980a79f92eb81e057198ca7542377
elisa lee <elisa.my.lee@gmail.com>
2016-11-19 12:12:20 -0600

    add harvest controller

---
+++ public/scripts/controller/harvest.controller.js
@@ -0,0 +1,7 @@
+angular.module('growLogApp')
+       .controller('HarvestController', HarvestController);
+
+function HarvestController() {
+  var harvest = this;
+  console.log('HarvestController loaded');
+}
