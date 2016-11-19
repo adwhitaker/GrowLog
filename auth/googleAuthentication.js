99ce17640ffbfaebd89b1b3ae2bb44daf4759717
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-19 11:05:29 -0600

    add comments

--- auth/googleAuthentication.js
+++ auth/googleAuthentication.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const passport = require('passport');
 
+// allows access to google profile info
 router.get('/',
   passport.authenticate('google', {
     scope: ['profile', 'email'],
     prompt: 'select_account',
-    accessType: 'offline'
+    // accessType: 'offline'
   }
 ));
 
+// after the user is authenticated they are sent here
 router.get('/callback',
   passport.authenticate('google', {
     successRedirect: '/home',
