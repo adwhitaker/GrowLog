9da47e98fbe1717ced127d61cef6c5ad84f38672
elisa lee <elisa.my.lee@gmail.com>
2016-11-19 11:47:52 -0600

    add activity controller

---
+++ public/scripts/controller/activity.controller.js
@@ -0,0 +1,7 @@
+angular.module('growLogApp')
+       .controller('ActivityController', ActivityController);
+
+function ActivityController() {
+  var activity = this;
+  console.log('ActivityController loaded');
+};
