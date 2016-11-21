4735c3d44c8c62ae7a66846b6106e877d614e95f
Jacquelyn Torborg <jtorborg1227@comcast.net>
2016-11-21 16:30:35 -0600

    updated weed form controller

--- public/scripts/controller/weedForm.controller.js
+++ public/scripts/controller/weedForm.controller.js
@@ -4,4 +4,10 @@ angular.module('growLogApp')
 function WeedFormController() {
   var weedForm = this;
   console.log('WeedFormController loaded');
+
+  weedForm.addWeedingActivity = function(field, section, row, assign)  {
+    var data = {field: field, section: section, row: row, assign: assign};
+    console.log('data', data);
+
+  };
 }
