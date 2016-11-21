a7582d6df7b7227f16530994c58abc8de65ed0d2
Laura Abend <laura.l.abend@gmail.com>
2016-11-21 14:04:04 -0600

    add waterForm controller

---
+++ public/scripts/controller/waterForm.controller.js
@@ -0,0 +1,7 @@
+angular.module('growLogApp')
+       .controller('WaterFormController', WaterFormController);
+
+function WaterFormController() {
+  var plantForm = this;
+  console.log('WaterFormController loaded');
+}
