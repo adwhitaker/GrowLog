26bb7d35e7cbc32cb6d0c0ed101b52e30693ea40
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-21 16:59:25 -0600

    create, update, add, delete locations

---
+++ routes/location.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const config = require('../db/connections');
+const knex = require('knex')(config.development);
+
+router.route('/')
+      .get(getLocations)
+      .post(addLocation);
+
+router.route('/:id')
+      .put(updateLocation)
+      .delete(deleteLocation);
+
+function getLocations(req, res) {
+
+  knex.select()
+      .from('location')
+      .then(function (locations) {
+        console.log('locations', locations);
+        res.send(locations);
+      }).catch(function (err) {
+        console.log('Error Querying the DB', err);
+        res.sendStatus(500);
+      });
+};
+
+function addLocation(req, res) {
+
+  var newLocation = {
+    field: req.body.field,
+    section: req.body.section,
+    row: req.body.row,
+  };
+
+  knex.insert(newLocation)
+      .into('location')
+      .returning('*')
+      .then(function (location) {
+        console.log('location', location);
+        res.sendStatus(200);
+      }).catch(function (err) {
+        console.log('Error Querying the DB', err);
+      });
+};
+
+function updateLocation(req, res) {
+  var locationID = req.params.id;
+
+  var updateLocation = {
+    field: req.body.field,
+    section: req.body.section,
+    row: req.body.row,
+  };
+
+  knex('location').where('id', locationID)
+                  .update(updateLocation)
+                  .then(function (response) {
+                    res.sendStatus(200);
+                  }).catch(function (err) {
+                    console.log('Error Querying the DB', err);
+                  });
+};
+
+function deleteLocation(req, res) {
+  var locationID = req.params.id;
+
+  knex('location').where('id', locationID)
+                  .delete()
+                  .then(function (response) {
+                    res.sendStatus(204);
+                  }).catch(function (err) {
+                    console.log('Error Querying the DB', err);
+                  });
+};
+
+module.exports = router;
