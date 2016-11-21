fe80814667babd852479a364a6c2b4384522ca6e
Jacquelyn Torborg <jtorborg1227@comcast.net>
2016-11-21 15:56:55 -0600

    added weedform

---
+++ public/scripts/controller/weedForm.controller.js
@@ -0,0 +1,7 @@
+angular.module('growLogApp')
+       .controller('WeedFormController', WeedFormController);
+
+function WeedFormController() {
+  var weedForm = this;
+  console.log('WeedFormController loaded');
+}
