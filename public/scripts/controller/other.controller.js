323ceb81e2e462ed0d2617f7cc8c10b4f9a6e4a2
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-21 15:23:17 -0600

    Added information to post to activity route

--- public/scripts/controller/other.controller.js
+++ public/scripts/controller/other.controller.js
@@ -1,7 +1,20 @@
 angular.module('growLogApp')
        .controller('OtherController', OtherController);
 
-function OtherController() {
-  var issues = this;
+function OtherController($http) {
+  var other = this;
   console.log('OtherController loaded');
+
+  other.addTask = function(task) {
+    var title = task.title;
+    var comments = task.comments;
+    var data = {title: title, comments: comments
+    };
+
+    $http.post('/activity', data);
+    // empty form after clicking 'Add task'
+    other.task = '';
+  }, function(error) {
+    console.log('Error posting request', error);
+  };
 }
