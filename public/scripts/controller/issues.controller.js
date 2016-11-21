7c5b93477554d3efef6c332ac23f4e2a82ea238d
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-21 15:59:04 -0600

    took out function that was in wrong controller

--- public/scripts/controller/issues.controller.js
+++ public/scripts/controller/issues.controller.js
@@ -4,17 +4,4 @@ angular.module('growLogApp')
 function IssuesController() {
   var issues = this;
   console.log('IssuesController loaded');
-
-  issues.addTask = function(task) {
-    var date = issues.date;
-    var title = issues.title;
-    var comments = issues.comment;
-    var data = {date: date, title: title; comments: comments;
-    }
-    $http.post('/activity', data);
-    // empty form after clicking 'Add task'
-    issues.task = '';
-  }, function(error) {
-    console.log('Error posting request', error);
-  };
 }
