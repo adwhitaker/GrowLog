7047dd0a3ed930da219df5a539963a31ff8c0111
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-21 15:59:32 -0600

    took out function that was in wrong controller

--- public/scripts/controller/other.controller.js
+++ public/scripts/controller/other.controller.js
@@ -1,20 +1,8 @@
 angular.module('growLogApp')
        .controller('OtherController', OtherController);
 
-function OtherController($http) {
+function OtherController() {
   var other = this;
   console.log('OtherController loaded');
 
-  other.addTask = function(task) {
-    var title = task.title;
-    var comments = task.comments;
-    var data = {title: title, comments: comments
-    };
-
-    $http.post('/activity', data);
-    // empty form after clicking 'Add task'
-    other.task = '';
-  }, function(error) {
-    console.log('Error posting request', error);
-  };
 }
