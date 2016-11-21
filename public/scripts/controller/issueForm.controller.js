d1a469f2cb08d47f33f1415689e1999ddd92a52e
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-21 15:58:36 -0600

    added function for form information

--- public/scripts/controller/issueForm.controller.js
+++ public/scripts/controller/issueForm.controller.js
@@ -1,7 +1,21 @@
 angular.module('growLogApp')
        .controller('IssueFormController', IssueFormController);
 
-function IssueFormController() {
+function IssueFormController($http) {
   var issueForm = this;
   console.log('IssueFormController loaded');
+  issueForm.addTask = function(task) {
+    var type = issue;
+    var assigndate = issueForm.date;
+    var title = issueForm.title;
+    var comments = issueForm.comment;
+    var data = {type: type, assigndate: date, title: title, comments: comments
+    };
+    $http.post('/activity', data);
+    // empty form after clicking 'Add task'
+    issues.task = '';
+  }, function(error) {
+    console.log('Error posting request', error);
+  };
+
 }
