20fcb09da4ba4feca254d01c5bf6eb5761bce78f
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-21 14:57:41 -0600

    Added issue form

---
+++ public/scripts/controller/issueForm.controller.js
@@ -0,0 +1,7 @@
+angular.module('growLogApp')
+       .controller('IssueFormController', IssueFormController);
+
+function IssueFormController() {
+  var issueForm = this;
+  console.log('IssueFormController loaded');
+}
