eff83b641d4c66e7a94ab73d6b99d4108396a1f9
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-21 14:11:23 -0600

    Added other form controller

---
+++ public/scripts/controller/otherForm.controller.js
@@ -0,0 +1,7 @@
+angular.module('growLogApp')
+       .controller('OtherFormController', OtherFormController);
+
+function OtherFormController() {
+  var otherForm = this;
+  console.log('OtherFormController loaded');
+}
