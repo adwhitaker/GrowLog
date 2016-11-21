191eddfa77dd116daf07ee6b755ba753a775db5d
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-21 16:00:02 -0600

    Added function to get information from form to DB

--- public/scripts/controller/otherForm.controller.js
+++ public/scripts/controller/otherForm.controller.js
@@ -1,7 +1,22 @@
 angular.module('growLogApp')
        .controller('OtherFormController', OtherFormController);
 
-function OtherFormController() {
+function OtherFormController($http) {
   var otherForm = this;
   console.log('OtherFormController loaded');
+
+  otherForm.addTask = function(task) {
+    console.log('task', task);
+    var type = other;
+    var title = task.title;
+    var comments = task.comments;
+    var data = {type: type, title: title, comments: comments
+    };
+
+    $http.post('/activity', data);
+    // empty form after clicking 'Add task'
+    other.task = '';
+  }, function(error) {
+    console.log('Error posting request', error);
+  };
 }
