5b1b6284b3dd08c98c741d3c69bbf7c2a20e8e6a
elisa lee <elisa.my.lee@gmail.com>
2016-11-22 11:26:55 -0600

    change indentation

--- public/scripts/services/seeds.service.js
+++ public/scripts/services/seeds.service.js
@@ -1,24 +1,19 @@
-angular.module('growLogApp').service('seedsService', seedsService);
-
-
-
-
+angular.module('growLogApp')
+       .service('seedsService', seedsService);
 
 function seedsService($http) {
   var ctrl = this;
-  console.log("seedsservice");
-  ctrl.getSeeds = function() {
-          return $http.get('/addSeed').then(function(response) {
-              console.log('response', response);
-              return response;
-          });
-      } //end of getSeeds
-      ctrl.addUsedSeed = function(seedsdata) {
-        console.log("seedsdata", seedsdata);
 
-              return $http.post('/seedsInUse', seedsdata).then(function(response) {
-                  console.log('response', response);
-                  return response;
-              });
-          } //end of addUsedSeeds
+  ctrl.getSeeds = function () {
+    return $http.get('/addSeed').then(function (response) {
+      return response;
+    });
+  };
+
+  ctrl.addUsedSeed = function (seedsdata) {
+    return $http.post('/seedsInUse', seedsdata).then(function (response) {
+      console.log('response', response);
+      return response;
+    });
+  }; //end of addUsedSeeds
 } //end of seedsService
