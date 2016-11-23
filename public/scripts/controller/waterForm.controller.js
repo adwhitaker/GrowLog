7c2d459391b4455df1eccd037eb420335076dbe1
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-23 14:50:01 -0600

    getting location to post to DB

--- public/scripts/controller/waterForm.controller.js
+++ public/scripts/controller/waterForm.controller.js
@@ -5,13 +5,13 @@ function WaterFormController($http, locationService) {
   var waterForm = this;
   console.log('WaterFormController loaded');
 
-  locationService.getLocations().then(function (response) {
+  locationService.getLocations().then(function(response) {
     waterForm.locations = response;
     console.log(response);
   });
 
   waterForm.addActivity = function(activity) {
-    var location_id = activity.location_id;
+    var location_id = activity.location.id;
     var assigndate = moment(activity.date).format('L');
     var data = {location_id: location_id, type: 'water', assigndate: assigndate
     };
