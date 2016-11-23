9250c4996b4b4b4dad735e997f6739c77fd47721
Laura Abend <laura.l.abend@gmail.com>
2016-11-23 11:41:05 -0600

    add submitPlant function and require location service

--- public/scripts/controller/plantForm.controller.js
+++ public/scripts/controller/plantForm.controller.js
@@ -1,10 +1,28 @@
 angular.module('growLogApp')
        .controller('PlantFormController', PlantFormController);
 
-function PlantFormController() {
+function PlantFormController($http, locationService) {
   var plantForm = this;
   console.log('PlantFormController loaded');
+
+  locationService.getLocations().then(function(response) {
+    plantForm.locations = response;
+  });
+
   plantForm.submitPlant = function(plant) {
-    console.log(plant);
+    var plantedassigndate = moment(plant.assignDate).format('L');
+    var choice = plant.choice;
+    var projectedharvestdate = moment(plant.harvestDate).format('L');
+    var quantity = plant.quantity;
+    var transfer = plant.transfer;
+    var location_id = plant.location.id;
+    var data = {plantedassigndate: plantedassigndate, choice: choice,
+      projectedharvestdate: projectedharvestdate, quantity: quantity,
+      transfer: transfer, location_id: location_id};
+    $http.post('/seedsInUse', data);
+
+    plantForm.plant = '';
+  }, function(error) {
+    console.log('Error posting request', error);
   };
 }
