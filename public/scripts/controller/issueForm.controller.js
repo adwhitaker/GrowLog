3b77968aa0eca250aff3ee9e66e9d8c53ec61dff
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-23 10:14:35 -0600

    Added location

--- public/scripts/controller/issueForm.controller.js
+++ public/scripts/controller/issueForm.controller.js
@@ -1,15 +1,21 @@
-angular.module('growLogApp')
+, angular.module('growLogApp')
        .controller('IssueFormController', IssueFormController);
 
-function IssueFormController($http) {
+function IssueFormController($http, locationService) {
   var issueForm = this;
   console.log('IssueFormController loaded');
+  locationService.getLocations().then(function(response) {
+    issueForm.locations = response;
+    console.log(response);
+  });
+  
   issueForm.addTask = function(task) {
     console.log('task', task);
+    var location = task.location_id;
     var assigndate = task.date;
     var title = task.title;
-    var comments = task.comment;
-    var data = {assigndate: date, title: title, comments: comments
+    var comments = task.comments;
+    var data = {location: location, type: 'issues', assigndate: assigndate, title: title, comments: comments
     };
     $http.post('/activity', data);
 
