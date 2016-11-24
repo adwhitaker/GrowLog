825870ef27dfcc0c521e530dd792f210d2e0246c
Laura Abend <laura.l.abend@gmail.com>
2016-11-24 14:44:39 -0600

    add seedService to plant controller

--- public/scripts/controller/plant.controller.js
+++ public/scripts/controller/plant.controller.js
@@ -1,7 +1,10 @@
 angular.module('growLogApp')
        .controller('PlantController', PlantController);
 
-function PlantController() {
+function PlantController($http, seedsService) {
   var plant = this;
   console.log('PlantController loaded');
+
+  plant.usedSeeds = seedsService;
+
 }
