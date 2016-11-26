027f40850032cb4718b6dcd73272c2bcc6dfcd04
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-26 17:25:40 -0600

    Fixed data structure

--- public/scripts/controller/other.controller.js
+++ public/scripts/controller/other.controller.js
@@ -1,8 +1,12 @@
 angular.module('growLogApp')
        .controller('OtherController', OtherController);
 
-function OtherController() {
+function OtherController(activityService) {
   var other = this;
   console.log('OtherController loaded');
+
+  activityService.getActivities().then(function(response) {
+    other.listActiveTasks = activityService.activitiesObject.other;
+  });
 
 }
