2907556c2fc9c3e8020dcad9615b00c78d772bc4
elisa lee <elisa.my.lee@gmail.com>
2016-11-25 18:37:06 -0600

    send over location id data

--- public/scripts/controller/plantForm.controller.js
+++ public/scripts/controller/plantForm.controller.js
@@ -13,18 +13,18 @@ function PlantFormController($http, locationService, seedsService) {
   locationService.getLocations().then(function (response) {
     plantForm.locations = response;
   });
-  
+
   plantForm.submitPlant = function(plant) {
     var seedsId = plant.choice.id;
-        console.log(seedsId);
     var plantedassigndate = moment(plant.assignDate).format('L');
     var projectedharvestdate = moment(plant.harvestDate).format('L');
     var quantity = plant.quantity;
     var transfer = plant.transfer;
-    var locationId = plant.location.id;
+    var location_id = plant.location.id;
+    console.log(location_id);
     var data = { seedsId: seedsId, transfer: transfer, plantedassigndate: plantedassigndate,
                 projectedharvestdate: projectedharvestdate, quantity: quantity,
-                locationId: locationId, };
+                location_id: location_id, };
     $http.post('/seedsInUse', data);
 
     plantForm.plant = '';
