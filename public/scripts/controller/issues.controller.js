734405e2cef5116fd07c184a22b4167eb6cc3ea5
elisa lee <elisa.my.lee@gmail.com>
2016-11-25 21:17:09 -0600

    get current issues

--- public/scripts/controller/issues.controller.js
+++ public/scripts/controller/issues.controller.js
@@ -1,7 +1,12 @@
 angular.module('growLogApp')
        .controller('IssuesController', IssuesController);
 
-function IssuesController() {
+function IssuesController(activityService) {
   var issues = this;
+
+  activityService.getActivities().then(function (response) {
+    issues.listActiveIssues = activityService.activitiesObject.issues;
+  });
+
   console.log('IssuesController loaded');
 }
