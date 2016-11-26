ee87a0c73fa4e6d69c5edb9d1c8ce9486d723bc8
Jacquelyn Torborg <jtorborg1227@comcast.net>
2016-11-26 09:36:08 -0600

    display weed data on DOM

--- public/scripts/controller/weed.controller.js
+++ public/scripts/controller/weed.controller.js
@@ -1,7 +1,20 @@
 angular.module('growLogApp')
        .controller('WeedController', WeedController);
-
-function WeedController() {
+function WeedController($http) {
   var weed = this;
+  weed.weedArray;
   console.log('WeedController loaded');
-}
+  weed.getActivities = function() {
+      $http.get('/activity').then(function(response){
+      weed.weedArray = response.data;
+      console.log(weed.weedArray);
+      weed.weedArray.assigndate = moment(weed.weedArray.assigndate).format('L');
+      weed.weedArray.completedate = moment(weed.weedArray.assigndate).format('L');
+
+      console.log('formatted date with moment', weed.weedArray.assigndate);
+    }, function(error) {
+      console.log('Error posting request', error);
+    });
+//end of getActivities
+};
+};//end of weedcontroller
