a607f583e67c5d81dabe31bf3e6d1a2af1ae4315
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-27 11:34:07 -0600

    added functionality to show tasks on DOM

--- public/scripts/controller/water.controller.js
+++ public/scripts/controller/water.controller.js
@@ -1,7 +1,11 @@
 angular.module('growLogApp')
        .controller('WaterController', WaterController);
 
-function WaterController() {
+function WaterController(activityService) {
   var water = this;
   console.log('WaterController loaded');
+
+  activityService.getActivities().then(function(response) {
+    water.listActiveIssues = activityService.activitiesObject.water;
+  });
 }
