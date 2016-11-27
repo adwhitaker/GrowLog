6e7c74af3d8b07e274a654367b908d1efe920b56
elisa lee <elisa.my.lee@gmail.com>
2016-11-27 13:46:38 -0600

    add delete and complete function

--- public/scripts/controller/issues.controller.js
+++ public/scripts/controller/issues.controller.js
@@ -1,12 +1,47 @@
 angular.module('growLogApp')
        .controller('IssuesController', IssuesController);
 
-function IssuesController(activityService) {
+function IssuesController(activityService, $route) {
   var issues = this;
 
   activityService.getActivities().then(function (response) {
     issues.listActiveIssues = activityService.activitiesObject.issues;
+    console.log(issues.listActiveIssues);
   });
 
+  issues.refresh = function () {
+    $route.reload();
+  };
+
+  //delete issue
+  issues.deleteIssue = function (activityId) {
+    activityService.deleteActivity(activityId).then(issues.refresh());
+  };
+
+  issues.completeIssue = function(id, issueObject) {
+    var id = id;
+    var completeDate = new Date();
+    var dd = completeDate.getDate();
+    var mm = completeDate.getMonth() + 1;
+    var yyyy = completeDate.getFullYear();
+    var completeDate = yyyy + '-' + mm + '-' + dd;
+
+
+    var activity = {
+      location_id: issueObject.location_id,
+      type: 'issues',
+      assigndate: issueObject.assigndate,
+      completedate: completeDate,
+      title: issueObject.title,
+      comments: issueObject.comments,
+      joins_id: issueObject.id,
+      users_id: issueObject.users_id,
+    };
+    console.log(activity);
+
+    activityService.updateActivity(id, activity).then(issues.refresh());
+  };
+
   console.log('IssuesController loaded');
+
 }
