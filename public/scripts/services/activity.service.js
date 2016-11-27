1b10cea51a527d04e4d7dce593e9174424d08823
elisa lee <elisa.my.lee@gmail.com>
2016-11-27 14:13:04 -0600

    edit deleteActivity function parameters

--- public/scripts/services/activity.service.js
+++ public/scripts/services/activity.service.js
@@ -71,9 +71,8 @@ function activityService($http) {
   };
 
   // deletes activity in the DB
-  activity.deleteActivity = function (data) {
-    console.log('data', data);
-    return $http.delete('/activity/' + data.id, data).then(function (response) {
+  activity.deleteActivity = function (activityId, joinsId) {
+    return $http.delete('/activity/' + activityId, joinsId).then(function (response) {
       console.log('response', response);
       activity.getActivities();
       return;
