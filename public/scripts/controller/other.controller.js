95512ebe000a86b471c9490744bdd5b47ae6b1ac
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-27 18:12:20 -0600

    Changed to match issues controller

--- public/scripts/controller/other.controller.js
+++ public/scripts/controller/other.controller.js
@@ -1,12 +1,38 @@
 angular.module('growLogApp')
        .controller('OtherController', OtherController);
 
-function OtherController(activityService) {
+function OtherController(activityService, $route) {
   var other = this;
   console.log('OtherController loaded');
 
   activityService.getActivities().then(function(response) {
     other.listActiveTasks = activityService.activitiesObject.other;
   });
+  other.refresh = function() {
+    $route.reload();
+  };
+  // delete
+  other.deleteOther = function(activityId, joinsId) {
+    activityService.deleteActivity(activityId, joinsId).then(other.refresh());
+  };
+  other.completeOther = function(id, otherObject) {
+    var id = id;
+    var completeDate = new Date();
+    var yyyy = completeDate.getFullYear();
+    var mm = completeDate.getMonth() + 1;
+    var dd = completeDate.getDate();
+    var completeDate = yyyy + '-' + mm + '-' + dd;
 
+    var activity = {
+      location_id: otherObject.location_id,
+      type: 'other',
+      assigndate: otherObject.assigndate,
+      completedate: completeDate,
+      title: otherObject.title,
+      comments: otherObject.comments,
+      joins_id: otherObject.joins_id,
+      users_id: otherObject.users_id,
+    };
+    activityService.updateActivity(id, activity).then(other.refresh());
+  };
 }
