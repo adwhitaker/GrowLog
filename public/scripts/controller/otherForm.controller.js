1465e8ff052b060da27366225d67979a139e62f3
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-27 18:12:50 -0600

    Moved functions to other controller

--- public/scripts/controller/otherForm.controller.js
+++ public/scripts/controller/otherForm.controller.js
@@ -1,7 +1,7 @@
 angular.module('growLogApp')
        .controller('OtherFormController', OtherFormController);
 
-function OtherFormController($http, locationService, activityService) {
+function OtherFormController($http, locationService) {
   var otherForm = this;
   console.log('OtherFormController loaded');
 
@@ -22,35 +22,9 @@ function OtherFormController($http, locationService, activityService) {
       title: task.title,
       comments: task.comments
     };
-
-    activityService.addActivity(data).then(function(response) {
-      // empty form after clicking 'Add task'
-      otherForm.task = '';
-    });
-
+    $http.post('/activity', data);
+    otherForm.task = '';
   }, function(error) {
     console.log('Error posting request', error);
   };
-
-  // updating tasks
-  otherForm.updateTask = function(task) {
-    var id = task.id;
-
-    var data = {
-      location_id: task.location.id,
-      type: 'other',
-      assigndate: moment(task.date).format('L'),
-      title: task.title,
-      comments: task.comments
-    };
-    activityService.updateActivity(id, data).then(function(response) {
-      // get activities runs in service
-    });
-  };
-  // delete task
-  otherForm.deleteTask = function(id) {
-    activityService.deleteActivity(id).then(function(response) {
-      // get activities runs in the service
-    });
-  };
-};
+}
