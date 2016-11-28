52dd8c6f0cad01e22b4bd31a08c270f940c48674
Laura Abend <laura.l.abend@gmail.com>
2016-11-28 13:38:17 -0600

    add completePlant function

--- public/scripts/controller/plant.controller.js
+++ public/scripts/controller/plant.controller.js
@@ -6,5 +6,26 @@ function PlantController($http, seedsService) {
   console.log('PlantController loaded');
 
   plant.usedSeeds = seedsService;
-  console.log('plant.usedSeeds', plant.usedSeeds);
+
+  plant.completePlant = function(id, plantObject) {
+    var id = id;
+    var completeDate = moment().format('L');
+
+    var completedPlant = {
+      seeds_id: plantObject.seeds_id,
+      transfer: plantObject.transfer,
+      quantity: plantObject.quantity,
+      plantedassigndate: plantObject.plantedassigndate,
+      plantdate: completeDate,
+      plantduration: plantObject.plantduration,
+      projectedharvestdate: plantObject.projectedharvestdate,
+      actualharvestdate: plantObject.actualharvestdate,
+      amountharvested: plantObject.amount,
+      amountharvestedunits: plantObject.units,
+      joins_id: plantObject.id,
+      location_id: plantObject.location_id,
+      seedsinuse_id: plantObject.seedsinuse_id,
+    };
+    seedsService.updateUsedSeed(completedPlant);
+  };
 }
