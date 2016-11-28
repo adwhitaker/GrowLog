f7ac2a345e0b84433d9b0f9fb95a3585163a98d6
elisa lee <elisa.my.lee@gmail.com>
2016-11-28 16:00:30 -0600

    display on DOM and edit water info

--- public/scripts/controller/water.controller.js
+++ public/scripts/controller/water.controller.js
@@ -1,28 +1,20 @@
 angular.module('growLogApp')
        .controller('WaterController', WaterController);
 
-function WaterController(activityService, $route) {
+function WaterController(activityService, $scope) {
   var water = this;
   console.log('WaterController loaded');
+  water.activities = activityService;
 
-  activityService.getActivities().then(function(response) {
-    water.listActiveIssues = activityService.activitiesObject.water;
-  });
+  activityService.getActivities();
 
-  water.refresh = function() {
-    $route.reload();
+  water.deleteWater = function (activityId, joinsId) {
+    activityService.deleteActivity(activityId, joinsId)
   };
-  // delete
-  water.deleteWater = function(activityId, joinsId) {
-    activityService.deleteActivity(activityId, joinsId).then(water.refresh());
-  };
+
   water.completeWater = function(id, waterObject) {
     var id = id;
-    var completeDate = new Date();
-    var yyyy = completeDate.getFullYear();
-    var mm = completeDate.getMonth() + 1;
-    var dd = completeDate.getDate();
-    var completeDate = yyyy + '-' + mm + '-' + dd;
+    var completeDate = moment().format('L');
 
     var activity = {
       location_id: waterObject.location_id,
@@ -34,6 +26,25 @@ function WaterController(activityService, $route) {
       joins_id: waterObject.id,
       users_id: waterObject.users_id,
     };
-    activityService.updateActivity(id, activity).then(water.refresh());
+    console.log(activity);
+
+    activityService.updateActivity(id, activity);
+  };
+
+  water.updateWater = function (id, waterObject) {
+    var id = id;
+    var newDate = moment(waterObject.newAssignDate).format('L');
+    console.log(newDate);
+    var activity = {
+      location_id: waterObject.location_id,
+      type: 'water',
+      assigndate: newDate,
+      comments: waterObject.comments,
+      joins_id: waterObject.id,
+      users_id: waterObject.users_id,
+    };
+    console.log(activity);
+
+    activityService.updateActivity(id, activity);
   };
 }
