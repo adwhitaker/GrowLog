dfaac0ad970919c24d7a113247aa74037bec13fb
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-27 18:13:20 -0600

    changed to match issues controller

--- public/scripts/controller/water.controller.js
+++ public/scripts/controller/water.controller.js
@@ -1,11 +1,39 @@
 angular.module('growLogApp')
        .controller('WaterController', WaterController);
 
-function WaterController(activityService) {
+function WaterController(activityService, $route) {
   var water = this;
   console.log('WaterController loaded');
 
   activityService.getActivities().then(function(response) {
     water.listActiveIssues = activityService.activitiesObject.water;
   });
+
+  water.refresh = function() {
+    $route.reload();
+  };
+  // delete
+  water.deleteWater = function(activityId, joinsId) {
+    activityService.deleteActivity(activityId, joinsId).then(water.refresh());
+  };
+  water.completeWater = function(id, waterObject) {
+    var id = id;
+    var completeDate = new Date();
+    var yyyy = completeDate.getFullYear();
+    var mm = completeDate.getMonth() + 1;
+    var dd = completeDate.getDate();
+    var completeDate = yyyy + '-' + mm + '-' + dd;
+
+    var activity = {
+      location_id: waterObject.location_id,
+      type: 'water',
+      assigndate: waterObject.assigndate,
+      completedate: completeDate,
+      title: waterObject.title,
+      comments: waterObject.comments,
+      joins_id: waterObject.id,
+      users_id: waterObject.users_id,
+    };
+    activityService.updateActivity(id, activity).then(water.refresh());
+  };
 }
