16d0e2606100970b06f8babf79d7bfaaaa60c6b5
Jacquelyn Torborg <jtorborg1227@comcast.net>
2016-11-29 10:37:49 -0600

    add angular-messages

--- build.sh
+++ build.sh
@@ -9,6 +9,11 @@ cp node_modules/angular/angular.min.js.map public/vendors;
 cp node_modules/angular-route/angular-route.min.js public/vendors;
 cp node_modules/angular-route/angular-route.min.js.map public/vendors;
 
+# angular-messages
+cp node_modules/angular-messages/angular-messages.min.js public/vendors;
+cp node_modules/angular-messages/angular-messages.min.js.map public/vendors;
+
+
 # angular-animate
 cp node_modules/angular-animate/angular-animate.min.js public/vendors;
 cp node_modules/angular-animate/angular-animate.min.js.map public/vendors;
