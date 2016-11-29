edd22d55e2840ab97e330cb5d1a4c4932322fa0a
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-29 10:03:49 -0600

    refactor code for readability and use seeds service to post

--- public/scripts/controller/newSeedForm.controller.js
+++ public/scripts/controller/newSeedForm.controller.js
@@ -1,38 +1,35 @@
 angular.module('growLogApp')
        .controller('NewSeedFormController', NewSeedFormController);
 
-function NewSeedFormController($http) {
+function NewSeedFormController($http, seedsService) {
   var newSeed = this;
   console.log('NewSeedFormController loaded');
 
-  newSeed.addSeed = function(seed) {
-    var generic = seed.generic;
-    var variety = seed.variety;
-    var family = seed.family;
-    var orderdate = seed.orderdate;
-    var quantity = seed.quantity;
-    var unitsperpack = seed.unitsperpack;
-    var quantityunits = seed.quantityunits;
-    var seedsperunit = seed.seedsperunit;
-    var manufacturer = seed.manufacturer;
-    var supplier = seed.supplier;
-    var daystoharvest = seed.daystoharvest;
-    var receivedate = seed.receivedate;
-    var lotnumber = seed.lotnumber;
-    var donation = seed.donation;
-    var plantouse = seed.plantouse;
-    var data = {generic: generic, variety: variety, family: family,
-      orderdate: orderdate, quantity: quantity, unitsperpack: unitsperpack,
-      quantityunits: quantityunits, seedsperunit: seedsperunit,
-      manufacturer: manufacturer, supplier: supplier,
-      daystoharvest: daystoharvest, receivedate: receivedate,
-      lotnumber: lotnumber, donation: donation, plantouse: plantouse
-    };
-    $http.post('/addSeed', data);
+  newSeed.addSeed = function (seed) {
 
-    // empty form after clicking 'submit'
-    newSeed.seed = '';
-  }, function(error) {
-    console.log('error posting request', error);
+    var data = {
+      generic: seed.generic,
+      variety: seed.variety,
+      family: seed.family,
+      orderdate: seed.orderdate,
+      quantity: seed.quantity,
+      unitsperpack: seed.unitsperpack,
+      quantityunits: seed.quantityunits,
+      seedsperunit: seed.seedsperunit,
+      manufacturer: seed.manufacturer,
+      supplier: seed.supplier,
+      daystoharvest: seed.daystoharvest,
+      receivedate: seed.receivedate,
+      lotnumber: seed.lotnumber,
+      donation: seed.donation,
+      plantouse: seed.plantouse
+    };
+
+    seedsService.addSeed(data)
+    .then(function () {
+      // empty form after clicking 'submit'
+      newSeed.seed = '';
+    });
+
   };
-}
+};
