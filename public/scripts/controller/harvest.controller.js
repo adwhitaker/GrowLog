626532be040ffbd475ffa64b83a27560667817d6
Alex Whitaker <whitaker.alexander@gmail.com>
2016-11-29 13:53:07 -0600

    basic complete

--- public/scripts/controller/harvest.controller.js
+++ public/scripts/controller/harvest.controller.js
@@ -1,7 +1,13 @@
 angular.module('growLogApp')
        .controller('HarvestController', HarvestController);
 
-function HarvestController() {
+function HarvestController(seedsService) {
   var harvest = this;
   console.log('HarvestController loaded');
-}
+
+  harvest.plantedSeeds = seedsService;
+
+  harvest.completeHarvest = function (harvestedPlant) {
+    console.log('harvest', harvestedPlant);
+  };
+};
