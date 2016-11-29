ea656a91dff86777ed7c4d7dcd82d3a72915d84a
elisa lee <elisa.my.lee@gmail.com>
2016-11-29 01:25:41 -0600

    add ngmessages

--- public/scripts/app.module.js
+++ public/scripts/app.module.js
@@ -1,1 +1,1 @@
-angular.module('growLogApp', ['ngRoute', 'ui.bootstrap', 'ngAnimate', 'ngMaterial']);
+angular.module('growLogApp', ['ngRoute', 'ui.bootstrap', 'ngAnimate', 'ngMaterial', 'ngMessages']);
