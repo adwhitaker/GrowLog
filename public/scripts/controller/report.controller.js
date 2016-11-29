be9353cadb65d55eb23679fa32f47b87e1b4180e
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-29 17:25:03 -0600

    working on report functionality

--- public/scripts/controller/report.controller.js
+++ public/scripts/controller/report.controller.js
@@ -1,7 +1,19 @@
 angular.module('growLogApp')
        .controller('ReportController', ReportController);
 
-function ReportController() {
+function ReportController($http) {
   var reportCtrl = this;
   console.log('ReportController loaded');
+  // reportCtrl.getInfo = function() {
+  //   $http.get('/reports').then(function(response) {
+  //     return response;
+  //   });
+  // };
+
+  reportCtrl.getInfo = function() {
+    return $http.get('/reports').then(function(response) {
+      console.log('resoponse', response);
+        return response.data;
+      });
+  };
 }
