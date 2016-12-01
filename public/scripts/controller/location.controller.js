4688ca36bffc810942a81148017b23387040fd62
elisa lee <elisa.my.lee@gmail.com>
2016-12-01 17:04:08 -0600

    add display shows or hides

--- public/scripts/controller/location.controller.js
+++ public/scripts/controller/location.controller.js
@@ -6,10 +6,46 @@ function LocationController(locationService, $route) {
 
   loc.locationsFromService = locationService;
 
-  // update location in DB
-  loc.updateLocation = function (id, field, section, row) {
-    data = { field: field, section: section, row: row };
+  loc.showDetails = function (id) {
+    loc['details' + id] = !loc['details' + id];
+    loc['edits' + id] = false;
+  };
 
+  loc.editDetails = function (id) {
+    loc['edits' + id] = !loc['edits' + id];
+    loc['details' + id] = false;
+  };
+
+  // update location in DB
+  loc.updateLocation = function (locationObject) {
+    loc.editDetails(locationObject.id);
+    console.log(locationObject);
+    if (locationObject.fieldUpdated === null) {
+      return;
+    } else {
+      locationObject.field = locationObject.fieldUpdated;
+    }
+
+    if (locationObject.sectionUpdated === null) {
+      return;
+    } else {
+      locationObject.section = locationObject.sectionUpdated;
+    }
+
+    if (locationObject.rowUpdated === null) {
+      return;
+    } else {
+      locationObject.row = locationObject.rowUpdated;
+    }
+
+    var id = locationObject.id;
+    var data = {
+      field: locationObject.field,
+      section: locationObject.section,
+      row: locationObject.row,
+    };
+
+    console.log(data);
     locationService.updateLocation(id, data)
     .then(function (response) {
 
@@ -24,4 +60,4 @@ function LocationController(locationService, $route) {
 
     });
   };
-};
+}
