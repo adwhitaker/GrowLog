ebc588bcc165acdcdb6099c93f79512b86092064
Alex Whitaker <whitaker.alexander@gmail.com>
2016-12-01 16:32:42 -0600

    refactor code, require locationservice

--- public/scripts/controller/weedForm.controller.js
+++ public/scripts/controller/weedForm.controller.js
@@ -4,40 +4,24 @@ angular.module('growLogApp')
 function WeedFormController($http, locationService, activityService) {
   var weedForm = this;
 
-    locationService.getLocations().then(function (response) {
-      weedForm.locations = response;
-      console.log(response);
-    });
+  weedForm.locationService = locationService;
 
+  // add a weeding activity to the DB
+  weedForm.addActivity = function (activity) {
 
-
-    weedForm.addActivity = function (activity) {
-console.log("activity", activity);
-var location_id = activity.location.id;
-console.log("id", location_id);
-var field = activity.location.field;
-console.log("field", field);
-
-var section = activity.location.section;
-console.log("section", section);
-
-var row = activity.location.row;
-console.log("row", row);
-
-var comments = activity.comments;
-console.log("comments", comments);
-
-var weedtype = activity.weedtype;
-
-var duration = activity.duration;
-
-var assigndate = moment(activity.date).format('L');
-
-var data = {location_id: activity.location.id, type: 'weed',  assigndate: assigndate, weedtype:weedtype, duration:duration, comments: comments}
-      $http.post('/activity', data);
-
-      weedForm.activity = '';
-    }, function(serror) {
-      console.log('Error posting request', error);
+    let data = {
+      location_id: activity.location.id,
+      type: 'weed',
+      assigndate: moment().format('L'),
+      weedtype: activity.weedtype,
+      comments: activity.comments
     };
-  }
+
+    activityService.addActivity(data)
+    .then(function () {
+      weedForm.activity = '';
+    }).catch(function (error) {
+      console.log('Error posting request', error);
+    });
+  };
+};
