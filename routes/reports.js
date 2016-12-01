06d1bf7301470b369f6e23c1cd329b01a843d140
Ally Boyd <alyssa.boyd85@gmail.com>
2016-12-01 13:49:16 -0600

    functional reporting

--- routes/reports.js
+++ routes/reports.js
@@ -11,9 +11,14 @@ function getReport(req, res) {
   // seeds.getSeeds().then(function(response) {
   //   return response;
   // });
-  knex.select()
-      .from('seeds')
-      .orderBy('id')
+  knex.select('field', 'section', 'row', 'type', 'assigndate', 'completedate', 'duration', 'amount', 'weedtype', 'title', 'comments', 'google_name', 'transfer', 'usedquantity', 'plantedassigndate', 'plantduration', 'projectedharvestdate', 'actualharvestdate', 'amountharvested', 'amountharvestedunits', 'harvestduration', 'generic', 'variety', 'family', 'orderdate', 'quantity', 'unitsperpack', 'quantityunits', 'seedsperunit', 'manufacturer', 'supplier', 'daystoharvest', 'receivedate', 'lotnumber', 'donation', 'plantouse')
+      .from('location')
+      .join('act_loc_users', 'location.id', 'act_loc_users.location_id')
+      .join('activities', 'act_loc_users.act_id', 'activities.id')
+      .join('users', 'act_loc_users.users_id', 'users.id')
+      .join('seeds_in_use_loc', 'location.id', 'seeds_in_use_loc.location_id')
+      .join('seedsinuse', 'seeds_in_use_loc.seedsinuse_id', 'seedsinuse.id')
+      .join('seeds', 'seedsinuse.seeds_id', 'seeds.id')
       .then(function (response) {
         console.log('response', response);
         res.send(response);
