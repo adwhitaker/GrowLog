2ff01ecd437255c0914936688a7217277026e4b4
elisa lee <elisa.my.lee@gmail.com>
2016-12-03 09:20:25 -0600

    add angular material and angular aria

--- build.sh
+++ build.sh
@@ -39,3 +39,11 @@ cp node_modules/angular-sanitize/angular-sanitize.min.js.map public/vendors;
 
 # ng-csv
 cp node_modules/ng-csv/build/ng-csv.min.js public/vendors;
+
+# angular-materials
+cp node_modules/angular-material/angular-material.min.css public/vendors;
+cp node_modules/angular-material/angular-material.min.js public/vendors;
+
+# angular-aria
+cp node_modules/angular-aria/angular-aria.min.js public/vendors
+cp node_modules/angular-aria/angular-aria.min.js.map public/vendors
