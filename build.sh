5e6298a0c54e8a0fae021f3387ea964f791b2254
elisa lee <elisa.my.lee@gmail.com>
2016-12-08 12:59:22 -0600

    remove bootstrap

--- build.sh
+++ build.sh
@@ -17,15 +17,15 @@ cp node_modules/angular-messages/angular-messages.min.js.map public/vendors;
 cp node_modules/angular-animate/angular-animate.min.js public/vendors;
 cp node_modules/angular-animate/angular-animate.min.js.map public/vendors;
 
-# angular ui bootstrap
-cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap-csp.css public/vendors;
-cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js public/vendors;
-cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js public/vendors;
-
-# bootstrap
-cp node_modules/bootstrap/dist/js/bootstrap.min.js public/vendors;
-cp node_modules/bootstrap/dist/css/bootstrap.min.css public/vendors;
-cp node_modules/bootstrap/dist/css/bootstrap.min.css.map public/vendors;
+# # angular ui bootstrap
+# cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap-csp.css public/vendors;
+# cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js public/vendors;
+# cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js public/vendors;
+#
+# # bootstrap
+# cp node_modules/bootstrap/dist/js/bootstrap.min.js public/vendors;
+# cp node_modules/bootstrap/dist/css/bootstrap.min.css public/vendors;
+# cp node_modules/bootstrap/dist/css/bootstrap.min.css.map public/vendors;
 
 # jQuery
 cp node_modules/jquery/dist/jquery.min.js public/vendors;
