00d472f03759a80dbc0ba5b879877f956f26e537
elisa lee <elisa.my.lee@gmail.com>
2016-12-15 15:14:04 -0600

    setup for heroku

--- db/connections.js
+++ db/connections.js
@@ -1,9 +1,19 @@
-// Knex.js DB setup
+const url = require('url');
+
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
+// DB setup for Knex.js
 module.exports = {
   development: {
     client: 'pg',
     connection: {
-      database: 'growlog'
-    }
-  }
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split('/')[1],
+      ssl: true,
+    },
+  },
 };
