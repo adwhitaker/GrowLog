96b91cada6ecbc1910d8b1453bf61c207f47c291
elisa lee <elisa.my.lee@gmail.com>
2016-12-16 09:03:11 -0600

    database config

--- db/connections.js
+++ db/connections.js
@@ -1,4 +1,3 @@
-const pg = require('pg');
 const url = require('url')
 
 const params = url.parse(process.env.DATABASE_URL);
@@ -7,14 +6,24 @@ const auth = params.auth.split(':');
 // DB setup for Knex.js
 module.exports = {
   development: {
-    client: 'pg',
-    connection: {
+    // client: 'pg',
+    // connection: {
       user: auth[0],
       password: auth[1],
       host: params.hostname,
       port: params.port,
       database: params.pathname.split('/')[1],
-      ssl: true,
-    },
-  },
+      ssl: true
+    }
+  // }
 };
+
+// // Knex.js DB setup
+// module.exports = {
+//   development: {
+//     client: 'pg',
+//     connection: {
+//       database: 'growlog'
+//     }
+//   }
+// };
